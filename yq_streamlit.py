03013cc6b948c29128750754f068c5fe3efb4074
GeraldKoh <141351058+GeraldKoh@users.noreply.github.com>
2023-08-10 20:46:04 +0800

    Update yq_streamlit.py

--- yq_streamlit.py
+++ yq_streamlit.py
@@ -1126,7 +1126,7 @@ with tab5:
     image = Image.open('Sydney_Image.jpg')
     st.image(image, caption='An Iconic View of Australia')
 
-    st.markdown("This tab allows predictions on Shift Sales of a shift based on the City and Shift ID. There are 2 predictions that we can make here: 1 on the current food truck business and 1 after menu optimization has been implemeted. The model used is a XGBoost model trained on the Tastybytes dataset.")
+    st.markdown("This tab allows predictions on Shift Sales of a shift based on the City and Shift ID. There are 2 predictions that we can make here: 1 on the current food truck business and 1 after menu optimization has been implemeted. The model used is a XGBoost model trained on the Tasty Bytes dataset.")
     st.write('Select City and Shift to get the predicted Shift Sales!')
 
     city_input = st.selectbox('Select a City', maintable['CITY'].unique(), key='city')
